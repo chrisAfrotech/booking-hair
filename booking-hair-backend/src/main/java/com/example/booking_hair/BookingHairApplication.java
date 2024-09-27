8b0ee1e0ff083a9fc137f28a7f227f5c0bef2f40
chrisAfrotech <tchiaguiachristophe25@gmail.com>
2024-09-27 17:47:53 +0100

    First commit

---
+++ booking-hair-backend/src/main/java/com/example/booking_hair/BookingHairApplication.java
@@ -0,0 +1,13 @@
+package com.example.booking_hair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingHairApplication.class, args);
+	}
+
+}
---
+++ booking-hair-backend/src/test/java/com/example/booking_hair/BookingHairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booking_hair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
